966e4fb6772dc45d531cf1d4edd29344cb2baf95
yousef500-1234 <61411664+yousef500-1234@users.noreply.github.com>
2020-02-24 16:11:16 +0200

    Buzzer code

--- DHT_Unified_Sensor.ino
+++ DHT_Unified_Sensor.ino
@@ -27,8 +27,39 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
 
 uint32_t delayMS;
 
+float computeHeatIndex(float temperature, float percentHumidity,bool isFahrenheit=1) {
+  float hi;
+
+
+  hi = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) +
+              (percentHumidity * 0.094));
+
+  if (hi > 79) {
+    hi = -42.379 + 2.04901523 * temperature + 10.14333127 * percentHumidity +
+         -0.22475541 * temperature * percentHumidity +
+         -0.00683783 * pow(temperature, 2) +
+         -0.05481717 * pow(percentHumidity, 2) +
+         0.00122874 * pow(temperature, 2) * percentHumidity +
+         0.00085282 * temperature * pow(percentHumidity, 2) +
+         -0.00000199 * pow(temperature, 2) * pow(percentHumidity, 2);
+
+    if ((percentHumidity < 13) && (temperature >= 80.0) &&
+        (temperature <= 112.0))
+      hi -= ((13.0 - percentHumidity) * 0.25) *
+            sqrt((17.0 - abs(temperature - 95.0)) * 0.05882);
+
+    else if ((percentHumidity > 85.0) && (temperature >= 80.0) &&
+             (temperature <= 87.0))
+      hi += ((percentHumidity - 85.0) * 0.1) * ((87.0 - temperature) * 0.2);
+  }
+
+  return hi;
+}
+
+
 void setup() {
   Serial.begin(9600);
+  pinMode(D2,OUTPUT);
   // Initialize device.
   dht.begin();
   Serial.println(F("DHTxx Unified Sensor Example"));
@@ -40,9 +71,9 @@ void setup() {
   Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
   Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
   Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
-  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("째C"));
-  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("째C"));
-  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("째C"));
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); 
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); 
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution);
   Serial.println(F("------------------------------------"));
   // Print humidity sensor details.
   dht.humidity().getSensor(&sensor);
@@ -58,28 +89,39 @@ void setup() {
   delayMS = sensor.min_delay / 1000;
 }
 
+float hd;
+float rel_h;
+float temp;
+/* 
+    rel_h=event.relative_humidity;
+    hd=computeHeatIndex(event.temperature,event.relative_humidity);
+*/
 void loop() {
-  // Delay between measurements.
-  delay(delayMS);
-  // Get temperature event and print its value.
   sensors_event_t event;
   dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error reading temperature!"));
+  if (isnan(event.temperature)){
+    Serial.println("couldn't read temp.");
+  }else{
+    temp=event.temperature;
   }
-  else {
-    Serial.print(F("Temperature: "));
-    Serial.print(event.temperature);
-    Serial.println(F("째C"));
+  if(isnan(event.relative_humidity)){
+    Serial.println("couldn't read hum.");
+  }else{
+    rel_h=event.relative_humidity;
   }
-  // Get humidity event and print its value.
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error reading humidity!"));
+  if(isnan(computeHeatIndex(temp,rel_h))){
+    Serial.println("couldn't read heatindex");
+  }else{
+    hd=computeHeatIndex(temp,rel_h);
   }
-  else {
-    Serial.print(F("Humidity: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F("%"));
+  Serial.print("Heat Index: ");Serial.println(hd);
+  Serial.print("rel_ hum: ");Serial.println(rel_h);
+  Serial.print("temp ");Serial.println(temp);
+  
+  if(hd>10){
+   Serial.println("BUZZZ");
+    digitalWrite(D2,1);
+    delay(1000);
+    digitalWrite(D2,0);
   }
 }
